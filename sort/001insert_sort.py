6453d116dbf7d82d5566ae9754c63398dee39efa
Kai Liu <liukaihitsz@163.com>
2018-04-23 16:44:36 +0800

    Create 001insert_sort.py

---
+++ sort/001insert_sort.py
@@ -0,0 +1,21 @@
+class(object):
+    def insert_sort(self,nums):
+        A = nums
+        
+        for i in range(1,len(nums)):
+            key = A[i]
+            j = i - 1
+            
+            while j>=0 and A[j] > key:
+                A[j+1] = A[j]
+                j -=1
+            
+            A[j+1] = key
+        return A
+
+def main():
+    nums = insert_sort([5,2,3,1,6,4])
+    print nums
+
+if "__name__" == "__main__":
+    main()
